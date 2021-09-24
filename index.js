6cad089bcaaf01a1a5eff4864be73c7654e34597
Sophie <sophiejoliver19+1@gmail.com>
2021-09-24 01:30:29 +0000

    calls both fetchMyIP and fetchCoordsByIP fns and logs whether or not those fns worked

---
+++ index.js
@@ -0,0 +1,22 @@
+// requires & runs the main fetch function
+
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('24.85.237.189', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+  
+// });
