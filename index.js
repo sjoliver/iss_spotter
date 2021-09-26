2186cfb47b64aee75dd483e49b5063225e7db92f
Sophie <sophiejoliver19+1@gmail.com>
2021-09-26 18:23:49 +0000

    created 3 functions with API calls to fetch IP address > coordinates > ISS flyover times

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // requires & runs the main fetch function
 
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -18,5 +18,13 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   }
 
 //   console.log('It worked! Returned coordinates:' , coordinates);
-  
+// });
+
+// fetchISSFlyOverTimes({latitude: '31234', longitude: '-1241'}, (error, times) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned flyover times:", times);
 // });
--- iss.js
+++ iss.js
@@ -39,7 +39,6 @@ const fetchCoordsByIP = function(ip, callback) {
       return;
     }
     
-  
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
       callback(Error(msg), null);
@@ -55,4 +54,60 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+//  * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+//  * Input:
+//  *   - An object with keys `latitude` and `longitude`
+//  *   - A callback (to pass back an error or the array of resulting data)
+//  * Returns (via Callback):
+//  *   - An error, if any (nullable)
+//  *   - The fly over times as an array of objects (null if error). Example:
+//  *     [ { risetime: 134564234, duration: 600 }, ... ]
+
+
+// input: latitude/longitute pair, an altitude, and how many results to return
+// output: get same inputs back, a time stamp when the API ran, a success/failure message, list of passes
+// each pass has a duration in seconds & a rise time as a unix time stamp
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}lon=${coords.longitude}`, (error, response, body) => {
+
+    // error handling - error present
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // error handling - repsonse codes
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    // parse out the response portion of the body
+    const passes = JSON.parse(body).response;
+    callback(error, passes);
+  });
+};
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results. 
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */ 
+ 
+const nextISSTimesForMyLocation = function(callback) {
+  
+  
+// callback(error, results);
+}
+
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
